ca7658faceecb8af9b33d23a480a666344e62eae
BugLizard <rosikokko@gmail.com>
2023-01-17 00:14:21 +0900

    最初のコミット

---
+++ src/pages/hurmony/top.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+
+const Top = () => {
+  return (
+    <Box>
+      <Text>test</Text>
+    </Box>
+  );
+};
+
+export default Top;
