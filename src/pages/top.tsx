679f357aff07b66d43a6cdd4baad2f4d2c51fc8d
BugLizard <rosikokko@gmail.com>
2023-01-19 23:51:22 +0900

    topのデザイン作成

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,12 +1,32 @@
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Button, Center, Text } from "@chakra-ui/react";
+import NextImage from "next/image";
 
 import React from "react";
 import logo from "../img/HurmonyTrans.png";
+//完成後余裕があったら動きをつける
 
 const Top = () => {
   return (
-    <Box boxSize="xl" background="#999">
-      <Image style={logo} alt="HurmonyLogo" />
+    <Box>
+      <Box mb="25px" display="flex" justifyContent="center">
+        <NextImage
+          src={logo}
+          className="HurmonyLogo"
+          alt="HurmonyLogo"
+          height="800"
+          width="800"
+        />
+      </Box>
+      <Center>
+        <Text>
+          舞台と音楽家を繋げるマッチングサイト、Hurmony（ヒューモニー）
+        </Text>
+      </Center>
+      <br />
+      <Center>
+        <Button>ログイン</Button>
+        <Button>新規登録</Button>
+      </Center>
     </Box>
   );
 };
