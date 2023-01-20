374132faa2f5f3f86731415d1318974e588ce4aa
BugLizard <rosikokko@gmail.com>
2023-01-21 03:16:50 +0900

    マッチングページデザイン途中経過

--- src/pages/matching.tsx
+++ src/pages/matching.tsx
@@ -1,10 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Avatar, Box, Center } from "@chakra-ui/react";
 import React from "react";
+
+//ハートだとこれみよがしすぎるので音符に変える
 
 const Matching = () => {
   return (
     <Box>
-      <></>
+      <Center>
+        <Box justifyContent="space-between" display="flex" marginTop="300px">
+          <Avatar name="pianoMan" size="2xl" />
+          <Avatar name="SopranoMan" size="2xl" />
+        </Box>
+      </Center>
     </Box>
   );
 };
